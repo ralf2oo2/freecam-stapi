8ca9d6ad94ef23ef21a9cd77c8fb962276049794
ralf2oo2 <ralf2oo2@hotmail.com>
2024-03-01 10:18:23 +0100

    added start of camera pos saving

---
+++ src/main/java/ralf2oo2/freecam/client/SaveManager.java
@@ -0,0 +1,13 @@
+package ralf2oo2.freecam.client;
+
+import ralf2oo2.freecam.util.CameraPosition;
+
+public class SaveManager {
+    public CameraPosition[] load(long seed, String worldName){
+        //TODO: add logic
+        return new CameraPosition[2];
+    }
+    public void save (long seed, String worldName, CameraPosition[] cameraPositions){
+        
+    }
+}
---
+++ src/main/java/ralf2oo2/freecam/util/SavedCameraPositions.java
@@ -0,0 +1,7 @@
+package ralf2oo2.freecam.util;
+
+public class SavedCameraPositions {
+    public long seed;
+    public String worldName;
+    public CameraPosition[] cameraPositions;
+}
