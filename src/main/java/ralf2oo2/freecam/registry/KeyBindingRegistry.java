5f80221b80fba53ffbe5c34fd2c33ca6b60f7348
Ralf2oo2 <ralf2oo2@hotmail.com>
2024-03-10 22:52:31 +0100

    replaced translation keys with words

--- src/main/java/ralf2oo2/freecam/registry/KeyBindingRegistry.java
+++ src/main/java/ralf2oo2/freecam/registry/KeyBindingRegistry.java
@@ -16,10 +16,10 @@ public class KeyBindingRegistry {
     @EventListener
     public void registerKeyBindings(KeyBindingRegisterEvent event) {
         List<KeyBinding> list = event.keyBindings;
-        list.add(freecamKeybinding = new KeyBinding("key.freecam.freecam", Keyboard.KEY_C));
-        list.add(playerMovementKeybinding = new KeyBinding("key.freecam.playermovement", Keyboard.KEY_V));
-        list.add(changeSpeedKeybinding = new KeyBinding("key.freecam.changespeed", Keyboard.KEY_B));
-        list.add(cameraPositionKeybinding = new KeyBinding("key.freecam.cameraposition", Keyboard.KEY_N));
-        list.add(cameraPositionGuiKeybinding = new KeyBinding("key.freecam.camerapositiongui", Keyboard.KEY_M));
+        list.add(freecamKeybinding = new KeyBinding("Toggle Freecam", Keyboard.KEY_C));
+        list.add(playerMovementKeybinding = new KeyBinding("Toggle Player Movement", Keyboard.KEY_V));
+        list.add(changeSpeedKeybinding = new KeyBinding("Change Freecam Speed", Keyboard.KEY_B));
+        list.add(cameraPositionKeybinding = new KeyBinding("Save Or Load Camera Position", Keyboard.KEY_N));
+        list.add(cameraPositionGuiKeybinding = new KeyBinding("Open Camera Position Gui", Keyboard.KEY_M));
     }
 }
